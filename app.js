ac8ce54c7f5ce88d1d76e9928e62104aa1d02fbd
Jared M. Smith <absynce@gmail.com>
2015-07-15 17:50:31 -0500

    Fixed date instantiation.

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ angular.module("singular", [])
         lastName: 'Owen',
         favoriteLanguage: 'C#',
         favoriteFramework: 'Angular 1.x',
-        codingSince: new Date('2011', '08', '17')
+        codingSince: new Date(2011, 08, 17)
       };
     }]);
